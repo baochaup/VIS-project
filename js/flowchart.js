4723c33d9593057964a109783d8c1c6dff65331a
Lucas Idstrom <31257219+u0571702@users.noreply.github.com>
2020-11-15 19:59:42 -0700

    Coloring and directional

--- js/flowchart.js
+++ js/flowchart.js
@@ -28,7 +28,6 @@ class FlowChart {
         let countryDict = {};
         let index = 0;
 
-
         this.migration.forEach((d) => {
             let size = countries.size
             if(countries.add(d.id).size > size){
@@ -45,56 +44,69 @@ class FlowChart {
             countryMatrix.push(arr.fill(0))
         }
 
+        let color = d3.scaleLinear()
+            .domain([0, 45])
+            .range([0, 1])
+
+
         let year = 2013;
 
         this.migration.forEach((d) => {
-            if(countries.has(d.od_id)){
-                if(!isNaN(d[year])){
-                    countryMatrix[countryDict[d.id].index][countryDict[d.od_id].index] = d[year];
-                }
+            if(d.type == "Immigrants"){
+                if(countries.has(d.od_id) && (d.id == "AU" || d.od_id == "AU")){
+                    if(!isNaN(d[year])){
+                        countryMatrix[countryDict[d.id].index][countryDict[d.od_id].index] = d[year];
 
-                //console.log(countryDict[d.id])
+                    }
+                }
             }
         });
 
-
-        // give this matrix to d3.chord(): it will calculates all the info we need to draw arc and ribbon
-        let res = d3.chord()
+        let calc = d3.chordDirected()
             .padAngle(0.02)     // padding between entities (black arc)
             .sortSubgroups(d3.descending)
             (countryMatrix)
 
-        // add the groups on the inner part of the circle
+        // Inner Part of circle
         this.chart
-            .datum(res)
-            .append("g")
+            .datum(calc)
+            .append("g").attr("id", "countryNames")
             .selectAll("g")
             .data(function(d) { return d.groups; })
             .enter()
-            .append("g").attr("transform", "translate(350, 350)")
+            .append("g").attr("transform", "translate(350, 350)").attr("class", "categories")
             .append("path")
-            .style("fill", "grey")
-            .style("stroke", "black")
+            //.style("fill", "grey")
+            .style("fill", d => d3.interpolateRainbow(color(d.index)))
+            .style("stroke", d => d3.rgb(d3.interpolateRainbow(color(d.index))).darker())
             .attr("d", d3.arc()
                 .innerRadius(300)
                 .outerRadius(310)
             )
 
-        // Add the links between groups
+        d3.selectAll(".categories").data(calc)
+            .each(d => { d.angle = (d.startAngle + d.endAngle) / 2;
+            console.log(d)})
+            .append("text")
+            .attr("dy", ".35em")
+            .attr("transform", d => "rotate(" + d.angle * 180 / Math.PI - 90 + ") " +
+                "translate(" + 300 + 26 + ")")
+            .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
+            .text("test");
+
+        //Add paths
         this.chart
-            .datum(res)
+            .datum(calc)
             .append("g").attr("transform", "translate(350, 350)")
             .selectAll("path")
             .data(function(d) { return d; })
             .enter()
             .append("path")
-            .attr("d", d3.ribbon()
+            .attr("d", d3.ribbonArrow()
                 .radius(300)
             )
-            .style("fill", "#69b3a2")
-
+            .style("fill", d => d3.interpolateRainbow(color(d.source.index)))
+            .style("stroke", d => d3.rgb(d3.interpolateRainbow(color(d.source.index))).darker())
+            .style("opacity", .8);
     }
-
-
-
 }
